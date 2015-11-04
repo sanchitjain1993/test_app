201ee8eab7dda966dd9751bac2ae1a55adae6104
Sanchit <jain.sanchit1993@gmail.com>
2015-11-04 12:40:20 +0000

    add flash, show errors, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,19 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
-        @article.save
+        if @article.save
+            flash[:notice] ="Article was created"
+            redirect_to article_path(@article)
+        else
+           render 'new' 
+        end
     end
+    
+    def show
+        @article = Article.find(params[:id])
+        
+    end
+    
     
     private
     def article_params
