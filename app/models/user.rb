3fabf7d1860cdf0186a413c992dcd683ab4a8c82
Sanchit <jain.sanchit1993@gmail.com>
2015-11-06 16:56:01 +0000

    one to many associations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    has_many :articles
     validates :username, presence: true,
                 uniqueness: { case_sensitive: false },
                 length: { minimum: 3, maximum: 25 }
