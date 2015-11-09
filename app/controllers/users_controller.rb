6cd357c16eb5c0b420fd3b770db11f7a0d4d6978
Sanchit <jain.sanchit1993@gmail.com>
2015-11-09 19:36:39 +0000

    added paginations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,16 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+    
+    def show
+        @user = User.find(params[:id])
+        @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+    end
+    
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5)
+    end
+
     def edit
         @user = User.find(params[:id])
     end
