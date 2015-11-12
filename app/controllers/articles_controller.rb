b634085f68421a0b7a671ca54f57dd6a202d1e40
Sanchit <jain.sanchit1993@gmail.com>
2015-11-12 18:57:43 +0000

    added admin functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,9 +43,9 @@ class ArticlesController < ApplicationController
             params.require(:article).permit(:title, :description)
         end
     def require_same_user
-        if current_user != @article.user
-        flash[:danger] = "You can only edit or delete your own articles"
-        redirect_to root_path
+        if current_user != @article.user and !current_user.admin?
+            flash[:danger] = "You can only edit or delete your own articles"
+            redirect_to root_path
         end
     end
 end
