9368b8864132a403330ac9f19380372fa7fa28ee
Sanchit <jain.sanchit1993@gmail.com>
2015-11-12 15:32:08 +0000

    restrict actions through controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,7 @@
 class WelcomeController < ApplicationController
     def home
-        
-        
-    end    
+            redirect_to articles_path if logged_in?
+    end
     
     def about
         
